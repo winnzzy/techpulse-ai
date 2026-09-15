e230c862c2bb5a00fe120cac6f1ddde5ccdfc6a4
Winner Nnamuah <nnamuahn@gmail.com>
2026-09-15 19:04:53 +0100

    Load decision framework styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import Link from "next/link";
 import { siteUrl } from "@/lib/site";
 import "./globals.css";
+import "./decision-framework.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
